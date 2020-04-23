530555851456b99b83702e48160d4d9ae15d523d
Mohammed Abed <abedmohammed2004@gmail.com>
2020-04-23 20:46:21 +0300

    test

---
+++ app/src/main/java/com/example/hasproject/Meat.kt
@@ -0,0 +1,77 @@
+package com.example.hasproject
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.fragment_fish_food.view.*
+
+
+class Meat : Fragment() {
+
+    class ProductFoodClass()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_fish_food, container, false)
+
+        val recyclerForProFood : RecyclerView = view.recycler_pro_food
+        recyclerForProFood.layoutManager = LinearLayoutManager(context)
+        val proFood = ArrayList<ProductFoodClass>()
+
+        proFood.add(ProductFoodClass())
+        proFood.add(ProductFoodClass())
+        proFood.add(ProductFoodClass())
+        proFood.add(ProductFoodClass())
+        proFood.add(ProductFoodClass())
+
+        val proFoodAdapter = MeatAdapter(proFood,context!!)
+        recyclerForProFood.adapter = proFoodAdapter
+
+
+
+        return view
+    }
+
+
+    class MeatAdapter(val pro_food_list: List<ProductFoodClass>,val context: Context) : RecyclerView.Adapter<MeatAdapter.MyViewHolder>() {
+
+
+
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): MyViewHolder {
+            val createView : View  = LayoutInflater.from(context).inflate(R.layout.product_food_list, p0 , false)
+            return MyViewHolder(createView)
+        }
+
+        override fun getItemCount(): Int {
+            return pro_food_list.size
+        }
+
+        override fun onBindViewHolder(p0: MyViewHolder, p1: Int) {
+        }
+
+
+        inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            var theNameOfPro: TextView
+            var theTypeOfPro: TextView
+            var theImageOfPro: ImageView
+            var thePriceOfPro: TextView
+
+            init {
+                theNameOfPro = itemView.findViewById(R.id.name_pro)
+                theTypeOfPro = itemView.findViewById(R.id.type_pro)
+                thePriceOfPro = itemView.findViewById(R.id.price_pro)
+                theImageOfPro = itemView.findViewById(R.id.image_pro)
+            }
+        }
+
+    }
+}
--- app/src/main/java/com/example/hasproject/ProductsFragment.kt
+++ app/src/main/java/com/example/hasproject/ProductsFragment.kt
@@ -3,15 +3,17 @@ package com.example.hasproject
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.products.*
+import kotlinx.android.synthetic.main.products.view.*
 
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -23,11 +25,42 @@ class ProductsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val viewToProduct = inflater.inflate(R.layout.products, container, false)
 
-        // Inflate the layout for this fragment
+        val adapter = MyViewPagerAdapter(fragmentManager!!)
+        adapter.addFragment(FishFood() , "Fish Food")
+        adapter.addFragment(Vegetables() , "Vegetables")
+        adapter.addFragment(Fruits() , "Fruits")
+        adapter.addFragment(Meat() , "Meat")
+        viewToProduct.viewPager.adapter = adapter
+        viewToProduct.tabs.setupWithViewPager(viewToProduct.viewPager)
+
+
         return viewToProduct
+    }
+
+
+    class MyViewPagerAdapter(manager:FragmentManager) : FragmentPagerAdapter(manager) {
+
+        private val fragmentList : MutableList<Fragment> = ArrayList()
+        private val titleList : MutableList<String> = ArrayList()
+
+        override fun getItem(p0: Int): Fragment {
+            return fragmentList[p0]
+        }
+
+        override fun getCount(): Int {
+            return fragmentList.size
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return titleList[position]
+        }
+
+        fun addFragment(fragment: Fragment , title:String) {
+            fragmentList.add(fragment)
+            titleList.add(title)
+        }
 
     }
 
